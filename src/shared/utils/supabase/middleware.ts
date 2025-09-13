4e6d30d79ecb85fa14ba0b0e860a06efddac04e1
Alabalala <allareiksa@gmail.com>
2025-09-13 19:13:08 +0200

    fix: add check also for sign up

--- src/shared/utils/supabase/middleware.ts
+++ src/shared/utils/supabase/middleware.ts
@@ -43,14 +43,15 @@ export async function updateSession(request: NextRequest) {
 		!request.nextUrl.pathname.startsWith("/login") &&
 		!request.nextUrl.pathname.startsWith("/auth")
 	) {
-		// no user, potentially respond by redirecting the user to the login page
 		const url = request.nextUrl.clone();
 		url.pathname = "/login";
 		return NextResponse.redirect(url);
 	}
 
-	if (user && request.nextUrl.pathname.startsWith("/login")) {
-		// user is signed in, but trying to access login page
+	if (
+		(user && request.nextUrl.pathname.startsWith("/login")) ||
+		request.nextUrl.pathname.startsWith("/sign-up")
+	) {
 		const url = request.nextUrl.clone();
 		url.pathname = "/";
 		return NextResponse.redirect(url);
