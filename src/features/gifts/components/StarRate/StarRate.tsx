7476f079f54be40fe5f16cb0c751343f1dc70c58
Alabalala <allareiksa@gmail.com>
2025-09-15 13:45:13 +0200

    fix: add missing key prop for list

--- src/features/gifts/components/StarRate/StarRate.tsx
+++ src/features/gifts/components/StarRate/StarRate.tsx
@@ -4,7 +4,12 @@ const StarRate = ({ rating }: { rating: string }) => {
 	return (
 		<div className={"flex gap-2 justify-around pointer-none "}>
 			{Array.from({ length: 5 }).map((_, index) => {
-				return <StarSVG filled={index + 1 <= Number(rating)} />;
+				return (
+					<StarSVG
+						key={index}
+						filled={index + 1 <= Number(rating)}
+					/>
+				);
 			})}
 		</div>
 	);
