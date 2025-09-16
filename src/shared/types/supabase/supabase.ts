1ad6a54feb233bc694efc89d41878450e4d3fcee
Alabalala <allareiksa@gmail.com>
2025-09-16 18:53:26 +0200

    feat: add supabase functions to write and read questions/answers & update gift function to retreive q&as

--- src/shared/types/supabase/supabase.ts
+++ src/shared/types/supabase/supabase.ts
@@ -1,3 +1,14 @@
-import { Database } from "../../../database.types";
+import { Database } from "../../../shared/types/database.types";
 
-export type Gift = Database["public"]["Tables"]["gifts"]["Row"];
+export type AnswerWithRow = Database["public"]["Tables"]["answers"]["Row"];
+
+export type QuestionWithAnswers =
+	Database["public"]["Tables"]["questions"]["Row"] & {
+		answers: AnswerWithRow[];
+	};
+
+export type Questions = Database["public"]["Tables"]["questions"]["Row"];
+export type Gift = Database["public"]["Tables"]["gifts"]["Row"] & {
+	profiles: { name: string } | null;
+	questions: QuestionWithAnswers[];
+};
