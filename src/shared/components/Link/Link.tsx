350d1ed4ebf3779876ea7e4bdef8b16e27a3ad40
Alabalala <allareiksa@gmail.com>
2025-09-18 11:50:06 +0200

    refactor: add styles for plain link

--- src/shared/components/Link/Link.tsx
+++ src/shared/components/Link/Link.tsx
@@ -1,22 +1,33 @@
 import { buttonVariants, variant } from "@/shared/styles/buttonVariant";
+import { plainButtonVariants } from "@/shared/styles/plainButtonVariants";
 import Link from "next/link";
 
 interface Props {
 	href: string;
 	children: React.ReactNode;
 	variant?: variant;
+	isPlain?: boolean;
 	floating?: boolean;
+	isGroup?: boolean;
+	size?: "default" | "small" | "icon";
 }
 
 export const NextLink = ({
 	href,
 	children,
+	isPlain = false,
 	variant,
 	floating = false,
+	size = "default",
+	isGroup = false,
 }: Props) => {
 	return (
 		<Link
-			className={buttonVariants({ variant: variant, floating: floating })}
+			className={
+				isPlain
+					? plainButtonVariants({ variant: variant, size: size, isGroup: isGroup })
+					: buttonVariants({ variant: variant, floating: floating })
+			}
 			href={href}
 		>
 			{children}
