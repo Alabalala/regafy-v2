db2245d1a0827d2192c9b653ba575d48df8fc87e
Alabalala <allareiksa@gmail.com>
2025-09-22 10:52:55 +0200

    feat: add use Profile to update store from supabase

---
+++ src/features/profile/store/hooks/useProfile.ts
@@ -0,0 +1,34 @@
+"use client";
+import { useEffect } from "react";
+import { useProfileStore } from "../profileStore";
+import { useUser } from "@/features/auth/hooks/useUser";
+import { getProfile } from "../../services/supabase";
+import { createClient } from "@/shared/services/supabase/client";
+import { useRouter } from "next/navigation";
+import { getPath } from "@/shared/services/getPath";
+
+export default function useProfile() {
+	const [user] = useUser();
+	const { profile, setProfile } = useProfileStore();
+	const supabase = createClient();
+	const router = useRouter();
+
+	useEffect(() => {
+		const fetchData = async () => {
+			if (!profile && user) {
+				try {
+					const fetchedProfile = await getProfile(user.id, supabase);
+					if (!fetchedProfile) {
+						router.push(getPath("New profile"));
+					}
+					setProfile(fetchedProfile);
+				} catch (err) {
+					console.log(err);
+				}
+			}
+		};
+		fetchData();
+	}, [profile, user]);
+
+	return [profile];
+}
