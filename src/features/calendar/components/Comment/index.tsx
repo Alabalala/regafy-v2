aa824631317148be186255f93185f164d93047a4
Alabalala <allareiksa@gmail.com>
2025-10-03 12:31:54 +0200

    style: change time and date to be below message

--- src/features/calendar/components/Comment/index.tsx
+++ src/features/calendar/components/Comment/index.tsx
@@ -33,14 +33,18 @@ const EventComment = ({
 						className={`flex ${isOwner ? "flex-row-reverse" : "flex-row"} items-center justify-between gap-4 ${isOwner ? "text-right" : "text-left"}`}
 					>
 						{profileName}
-						<span className="text-xs text-gray-600">
-							{getPrettyDate(new Date(messageTime), "en-UK", true)}
-						</span>
 					</p>
 					<div
-						className={`min-w-60 w-fit border-2 rounded-md p-3  ${isDescription ? "bg-accent dark:bg-accent-dark" : isOwner ? "bg-secondary dark:bg-secondary-dark text-right ml-auto mr-0" : " bg-tertiary dark:bg-tertiary-dark"}`}
+						className={`w-fit border-2 rounded-md p-3  ${isDescription ? "bg-accent dark:bg-accent-dark" : isOwner ? "bg-secondary dark:bg-secondary-dark text-right ml-auto mr-0" : " bg-tertiary dark:bg-tertiary-dark"}`}
 					>
 						<p>{message}</p>
+					</div>
+					<div
+						className={`flex flex-row gap-2 items-center ${isOwner ? "justify-end" : "justify-start"}`}
+					>
+						<span className={`text-[10px] text-gray-600 `}>
+							{getPrettyDate(new Date(messageTime), "en-UK", true)}
+						</span>
 					</div>
 				</div>
 			</div>
