3c1792baf4c4dc504f68f80307efc1f4ead68e04
Alabalala <allareiksa@gmail.com>
2025-10-25 19:48:03 +0200

    feature: add react form hook to login form

--- src/features/auth/components/LoginForm/LoginForm.tsx
+++ src/features/auth/components/LoginForm/LoginForm.tsx
@@ -5,90 +5,77 @@ import {
 	LOGIN_FORM_INITIAL_DATA,
 	LOGIN_FORM_INPUTS,
 } from "../../constants/forms";
-import { FieldErrors } from "@/shared/types/forms";
 import Input from "@/shared/components/Input";
 import { Button } from "@/shared/components/Button";
-import { validateLoginForm } from "../../services/validateLoginForm";
-import { login } from "../../services/supabase";
 import { useUserStore } from "@/features/auth/stores/userStore";
 import { useRouter } from "next/navigation";
 import { getPath } from "@/shared/services/getPath";
 import { NextLink } from "@/shared/components/Link";
+import { useForm } from "react-hook-form";
+import { LoginFormTypes } from "../../types/forms";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginFormSchema } from "../../schemas/loginForm";
+import { loginAction } from "../../actions/login";
 
 const LoginForm = () => {
-	const [formData, setFormData] = useState<LoginFormTypes>(
-		LOGIN_FORM_INITIAL_DATA,
-	);
-	const [errors, setErrors] = useState<FieldErrors>({});
 	const [supabaseToast, setsupabaseToast] = useState<string | undefined>("");
-	const [isLoading, setIsLoading] = useState(false);
-	const { user, setUser } = useUserStore();
+	const { user } = useUserStore();
 	const router = useRouter();
+
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors, isSubmitting },
+	} = useForm<LoginFormTypes>({
+		resolver: zodResolver(LoginFormSchema),
+		defaultValues: LOGIN_FORM_INITIAL_DATA,
+	});
 
 	useEffect(() => {
 		if (user) {
 			router.push(getPath("Home"));
 		}
-	}, [user]);
+	}, [user, router]);
 
-	const onChange = (
-		e: React.ChangeEvent<
-			HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
-		>,
-	) => {
-		const { name, value } = e.target as HTMLInputElement;
-		const normalisedValue = value.replace(/^ +/, "");
-
-		setFormData({
-			...formData,
-			[name]: normalisedValue,
-		});
-	};
-
-	const onSubmit = async () => {
-		setIsLoading(true);
-		const validationResult = validateLoginForm(formData);
-
-		if (!validationResult.success) {
-			setErrors(validationResult.errors);
-			setIsLoading(false);
+	const onSubmit = async (formData: LoginFormTypes) => {
+		const result = await loginAction(formData);
+		console.log(result);
+		if (!result.success) {
+			setsupabaseToast(result.errors?.root);
 			return;
 		}
 
-		setErrors({});
-
-		const supabaseResult = await login(formData);
-
-		if (!supabaseResult.success) {
-			setsupabaseToast(supabaseResult.error);
-			setIsLoading(false);
+		if (result.success) {
+			router.push(getPath("Home"));
 			return;
 		}
-
-		if (supabaseResult.user) {
-			setUser(supabaseResult.user);
-		}
-
-		router.push(getPath("Home"));
 	};
 
 	return (
-		<form className={"flex flex-col gap-4"}>
-			{LOGIN_FORM_INPUTS.map((input) => (
-				<div
-					key={input.name}
-					className={"flex flex-col gap-2"}
-				>
-					<p className={"font-bold"}>{input.label}</p>
-					<Input
-						onChange={onChange}
-						input={input}
-						value={formData[input.name as keyof LoginFormTypes]}
-						error={!!errors[input.name]?.length}
-					/>
-					<div className="text-red-500 text-sm">{errors[input.name]?.[0]}</div>
-				</div>
-			))}
+		<form
+			onSubmit={handleSubmit(onSubmit)}
+			className={"flex flex-col gap-4"}
+		>
+			{LOGIN_FORM_INPUTS.map((input) => {
+				const fieldName = input.name as keyof LoginFormTypes;
+				return (
+					<div
+						key={fieldName}
+						className={"flex flex-col gap-2"}
+					>
+						<p className={"font-bold"}>{input.label}</p>
+
+						<Input
+							{...register(fieldName)}
+							input={input}
+							currentValue={watch(fieldName) || ""}
+							error={!!errors[fieldName]}
+						/>
+						<div className="text-red-500 text-sm">{errors[fieldName]?.message}</div>
+					</div>
+				);
+			})}
 
 			{supabaseToast && (
 				<div className="text-red-500 text-sm">{supabaseToast}</div>
@@ -96,10 +83,10 @@ const LoginForm = () => {
 
 			<div className={"flex justify-center"}>
 				<Button
-					loading={isLoading}
-					loadingText="Logging in..."
-					type={"button"}
-					onClick={onSubmit}
+					type="submit"
+					disabled={isSubmitting}
+					loading={isSubmitting}
+					loadingText={"Logging in..."}
 				>
 					Log in
 				</Button>
