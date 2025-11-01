3adbd53d52679b7676b31f8a3af677e1f2b8527d
Alabalala <allareiksa@gmail.com>
2025-11-01 11:08:58 +0100

    feat: update middleware to find preferred langauge and default to en if none

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,27 @@
-import { type NextRequest } from "next/server";
-import { updateSession } from "@/shared/services/supabase/middleware";
+import { NextResponse, type NextRequest } from "next/server";
+import { updateSession } from "@/shared/services/supabase/updateSession";
 
 export async function middleware(request: NextRequest) {
-	return await updateSession(request);
+	const authResponse = await updateSession(request);
+	if (authResponse?.status === 302) return authResponse;
+
+	const pathname = request.nextUrl.pathname;
+	if (pathname === "/") {
+		const acceptLanguage = request.headers.get("accept-language");
+		const userLang = acceptLanguage?.split(",")[0].split("-")[0]; // e.g. "es" from "es-ES"
+		const supportedLocales = ["en", "es", "fr"];
+		const locale = supportedLocales.includes(userLang ?? "") ? userLang : "en";
+
+		const url = request.nextUrl.clone();
+		url.pathname = `/${locale}`;
+		return NextResponse.redirect(url);
+	}
+
+	return NextResponse.next();
 }
 
 export const config = {
 	matcher: [
-		/*
-		 * Match all request paths except for the ones starting with:
-		 * - _next/static (static files)
-		 * - _next/image (image optimization files)
-		 * - favicon.ico (favicon file)
-		 * Feel free to modify this pattern to include more paths.
-		 */
 		"/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
 	],
 };
