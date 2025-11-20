efdc9ddc1b1b9210ac8a23247e8d38853e460176
Alabalala <allareiksa@gmail.com>
2025-11-20 18:43:01 +0100

    chire: clean up unused files from regafy v1

--- src/shared/services/checkDate.js
+++
@@ -1,7 +0,0 @@
-export function checkDate(date) {
-  const eventDate = new Date(date)
-  const today = new Date();
-  today.setHours(0, 0, 0, 0);
-  eventDate.setHours(0, 0, 0, 0);
-  return today > eventDate;
-}
