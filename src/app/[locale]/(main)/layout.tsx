21aa1282a1481c1f224501c97bf6d9f84baafb43
Alabalala <allareiksa@gmail.com>
2025-11-20 13:21:41 +0100

    add overflow-hidden to parent container to see if it solves the issue with iphone

--- src/app/[locale]/(main)/layout.tsx
+++ src/app/[locale]/(main)/layout.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className={"flex flex-col h-dvh"}>
+		<div className={"flex flex-col h-dvh overflow-hidden"}>
 			<NavBar type={"user"} />
 			<ScrollReset></ScrollReset>
 			<main
