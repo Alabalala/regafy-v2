97218ec39e243feabb20b800e54fdd940a60336c
Álvaro <allareiksa@gmail.com>
2025-12-27 23:31:17 +0100

    fix: update code to always reflect changes in reservations

--- src/features/gifts/hooks/useChangeReserve.ts
+++ src/features/gifts/hooks/useChangeReserve.ts
@@ -2,27 +2,34 @@ import { useUser } from "@/features/auth/hooks/useUser";
 import { createClient } from "@/shared/services/supabase/client";
 import { Gift } from "@/shared/types/supabase/supabase";
 import { reserveGift } from "../services/supabase";
+import { Dispatch, SetStateAction } from "react";
 
 export function useChangeReserve(
-	gifts: Gift[],
-	setGifts: (gifts: Gift[]) => void,
+    gifts: Gift[],
+    setGifts: Dispatch<SetStateAction<Gift[]>>,
 ) {
-	const [user] = useUser();
-	const supabase = createClient();
+    const [user] = useUser();
+    const supabase = createClient();
 
-	const changeReserve = async (giftId: string) => {
-		let isReserved = false;
-		const updatedGifts = gifts.map((gift) => {
-			if (gift.id === giftId) {
-				isReserved = !gift.reserved;
-				return { ...gift, reserved: !gift.reserved };
-			}
-			return gift;
-		});
+    const changeReserve = async (giftId: string,) => {
+        const targetGift = gifts.find(g => g.id === giftId);
+        if (!targetGift || !user?.id) return;
 
-		setGifts(updatedGifts);
-		await reserveGift(giftId, supabase, isReserved, user?.id);
-	};
+        const nextValue = !targetGift.reserved;
+        const nextReservedBy = nextValue ? user.id : null;
 
-	return { changeReserve };
+        setGifts(prev => prev.map(g => 
+                    g.id === giftId 
+                        ? { ...g, reserved: nextValue, reserved_by: nextReservedBy } 
+                        : g
+                ));
+                
+        try {
+            await reserveGift(giftId, supabase, nextValue, user?.id);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return { changeReserve };
 }
